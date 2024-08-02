d0ca4b0d1828bb7809422eaedbdfe9bf04bb7d96
sarthak2400 <134042203+sarthak2400@users.noreply.github.com>
2024-08-02 16:48:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score=0 //agr hmm 33 ki "" ke ander likhge to ye string ban jayegi 
+
+//   console.log(typeof score);
+
+let valueInNumber = Number(score)    //Number likhne se ye confirm hoga ki score ki type ab number hi ayegi
+//  console.log(typeof (score))
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+//notes.... "33abc" covert into Nan
+//true covert into 1 and false in 0
+
+
+//boolean me covert karne ke liye 
+let isLogged=1       //1---->True .....0------->False   ....isko empty ("")rakhenge to false or name likhege to true 
+// conversion
+ // let booleanIsLoggedIn= Boolean(isLogged)
+//  console.log(booleanIsLoggedIn);    //----------the answer is true
+
+
+// string conversion 
+
+let someNumber =24
+
+//conversion 
+let stringNumber= String(someNumber)
+console.log(stringNumber)   
