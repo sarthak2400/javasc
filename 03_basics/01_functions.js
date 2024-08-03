1ee2682a952c8ba6de538ceea382355415669fc2
sarthak2400 <134042203+sarthak2400@users.noreply.github.com>
2024-08-03 23:32:15 +0000

    functions advance

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,15 +31,47 @@ const result =adddTwoNumber(3,5)           //ye to ans sahi dega
  function loginUserMessage(username){
 
     if(username === undefined){            //is line ki jgah (!username) likh do tab bhi same kam karega             //agr username undefined hoga to msg print karo please enter a username
-        console.log("please enter a username")
+     //   console.log("please enter a username")
     }
 
     return`${username} just logged in`
  }
  //console.log(loginUserMessage("sarthak"))
- console.log(loginUserMessage())          //undefined jab user kuch pass nhi karega 
+ //console.log(loginUserMessage())          //undefined jab user kuch pass nhi karega 
 
 //   function loginUserMessage(username ="sarthak"){          ese default value bhi pass kar skte hai 
+
+ // cl------2
+
+ function calCulatecartPrice(...num1){          //rest operator 
+return num1                                     // mtlb ab jitne marji number pass karo vo sab array me dalenge
+ }
+ console.log(calCulatecartPrice(200,600,700));
+
+ //object ko function me kese pass or use kiya jata hai 
+ const user ={
+    username:"sarthak",
+    price:199
+ }
+function handleObject(anyobject){
+    // console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+// handleObject(user)
+// handleObject({
+  //  username:"sam",
+   // price:399
+// })
+
+// arrays pass kar skte hai function me 
+
+const myNewArray =[200,700,600,1000]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+
+
 
  
 
