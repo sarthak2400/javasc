80abfac8c0f6aa6cfc0f04439d00bb60f50e16a9
sarthak2400 <134042203+sarthak2400@users.noreply.github.com>
2024-08-03 10:34:36 +0000

    array basic

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,54 @@
+// ARRAYS
+
+const myArray=[0,1,2,3,4,5]   //ese declare karte  hai 
+const myHeroes =["sarthak", "Ironman "]
+//console.log(myArray[0])
+
+
+// ARRAYS METHODS
+
+myArray.push(6)      // 6 bhi add ho gya 
+//console.log(myArray)
+myArray.pop(6)   // remove last elememt 
+//console.log(myArray)
+
+// unshift
+
+myArray.unshift(9)          //starting me insert ho gya 
+myArray.shift()
+// console.log(myArray);
+myArray.shift()              // starting me se 0 ud gya 
+//console.log(myArray)
+ 
+
+// question bhi puch sakte hai 
+// console.log(myArray.includes(9))
+// console.log(myArray.indexOf(3))
+
+// 
+const newArr = myArray.join()
+// console.log(myArray);                 //join ne hmare arraay ko bind or string me convert kar diya hai 
+// console.log(newArr);
+
+
+
+//slice , splice           IMP QUESTIONS
+
+console.log("A",myArray);
+
+const myn1 =myArray.slice(0,3)
+console.log(myn1);
+console.log("B", myArray);
+
+const myn2 = myArray.splice(0,3)
+console.log( "C",myArray);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
