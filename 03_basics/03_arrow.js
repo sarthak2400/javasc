b956c09ddb5b8886ecdd61a1ab741ee1fb5abf86
sarthak2400 <134042203+sarthak2400@users.noreply.github.com>
2024-08-04 11:30:38 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,11 +3,42 @@ const user ={
     price:499,
 
     welcomeMessage: function(){
-        console.log(`${this.username} ,welcome to website`);
+       // console.log(`${this.username} ,welcome to website`);
         
 
     }
 }
-user.welcomeMessage()
-user.username="sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username="sam"             //username ko change kar diya 
+// user.welcomeMessage()
+
+// function chai(){
+   // let username ="sarthak"
+    // console.log(this.username)
+//}
+//chai()
+
+
+//function declare through arrow 
+
+
+const chai= () => {                             //ban gya arrow function
+    let username ="sarthak"
+   // console.log(this.username)
+}
+chai()
+
+//PURE ARROW function               () => {}
+
+     // const addTwo=(num1, num2) => {
+       // return num1+num2
+   // }
+// console.log(addTwo(3,4))
+
+
+//  2 way implicit function return keywrod nhi likho jab ek hi line ka code hia 
+
+const addTwo=(num1,num2) => (num1+num2)
+console.log(addTwo(3,4))                          //ese bhi likh sakte hai 
+
+//      **************refer to notebook  for more imp info 
