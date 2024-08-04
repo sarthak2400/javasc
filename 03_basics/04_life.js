925f222f9c4ff47f4df14077bc602d0c387da6b0
sarthak2400 <134042203+sarthak2400@users.noreply.github.com>
2024-08-04 11:55:19 +0000

    life

---
+++ 03_basics/04_life.js
@@ -0,0 +1,10 @@
+//Immdiately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    console.log(`DB CONNECTED`)
+})(); 
+
+//1-() ye jha hmm functin ki defination likhne wale hai  2-() for execution call
+
+//globall scope ke pollution ko htane ke liye iife use karte hai 
+
